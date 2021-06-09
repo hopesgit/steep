0a8059a8b580538cbab3a6bd86b2d6a4e83768d8
Hope Gochnour <hope.gochnour@gmail.com>
2021-06-09 11:01:06 -0600

    Add basic endpoint for subscribing (or resubbing) to a tea

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::SubscriptionsController < ApplicationController
+  def create
+    sub = Subscription.find_by(customer_id: params[:customer_id], tea_id: params[:tea_id])
+    if sub
+      sub.status == 0
+    elsif !sub
+      new_sub = Subscription.create(
+        customer_id: params[:customer_id],
+        tea_id: params[:tea_id],
+        status: 0
+      )
+      render json: SubscriptionSerializer.new(new_sub).serialized_json
+    end
+  end
+end
---
+++ spec/requests/subscribe_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "When a POST request is sent to /api/v1/subscribe" do
+  describe "with the customer id and tea id" do
+    it 'creates the subscription' do
+      post '/api/v1/subscribe', params: { customer_id: 25, tea_id: 5 }
+
+      expect(response).to have_http_status(:success)
+
+      parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(parsed_response).to have_key(:data)
+      expect(parsed_response[:data]).to have_key(:id)
+      expect(parsed_response[:data]).to have_key(:type)
+      expect(parsed_response[:data]).to have_key(:attributes)
+      expect(parsed_response[:data][:attributes]).to have_key(:customer_id)
+      expect(parsed_response[:data][:attributes]).to have_key(:tea_id)
+      expect(parsed_response[:data][:attributes]).to have_key(:status)
+
+      expect(parsed_response[:data][:attributes][:status]).to eq("active")
+
+    end
+  end
+end

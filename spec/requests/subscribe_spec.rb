c30326866713cc04b9c33c85cdfd7c770d751c9a
Hope Gochnour <hope.gochnour@gmail.com>
2021-06-09 11:06:27 -0600

    "removing" coverage files

--- spec/requests/subscribe_spec.rb
+++ spec/requests/subscribe_spec.rb
@@ -11,12 +11,16 @@ describe "When a POST request is sent to /api/v1/subscribe" do
 
       expect(parsed_response).to have_key(:data)
       expect(parsed_response[:data]).to have_key(:id)
+      expect(parsed_response[:data][:id]).to_not be_nil
       expect(parsed_response[:data]).to have_key(:type)
+      expect(parsed_response[:data][:type]).to eq("subscription")
       expect(parsed_response[:data]).to have_key(:attributes)
       expect(parsed_response[:data][:attributes]).to have_key(:customer_id)
       expect(parsed_response[:data][:attributes]).to have_key(:tea_id)
       expect(parsed_response[:data][:attributes]).to have_key(:status)
 
+      expect(parsed_response[:data][:attributes][:customer_id]).to eq(25)
+      expect(parsed_response[:data][:attributes][:tea_id]).to eq(5)
       expect(parsed_response[:data][:attributes][:status]).to eq("active")
 
     end

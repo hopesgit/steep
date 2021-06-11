739e655b4c3cdf2ccad71cc4c3aeaa7ab9916ae6
Hope Gochnour <hope.gochnour@gmail.com>
2021-06-11 07:52:00 -0600

    Add foreign key attribute to subscriptions' customer_id column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_125354) do
+ActiveRecord::Schema.define(version: 2021_06_11_133626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_06_09_125354) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "subscriptions", "customers"
 end
